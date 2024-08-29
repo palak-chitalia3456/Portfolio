d7ccdf60eef226428db3165e5f0aeadbf8b849d9
palak-chitalia3456 <palakmuni214@gmail.com>
2024-08-29 21:22:05 +0530

    my_vite_app

--- React project/my_app/src/App.js
+++ React project/my_app/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
+//this file goes inside the root element i.e div element. whatever we write here is goint to render on root element through index.js
 
 function App() {
  // return (
@@ -20,6 +21,15 @@ function App() {
     //   </header>
     // </div>
   //);
+  return (<div><h2>jieee</h2> <div>
+    <div>
+      <h1>Hello</h1>
+      <h1>
+        Bye
+      </h1>
+    </div>
+  </div>
+  </div>); //this way of writing is jsx. i.e when we combine html code and js code together i.e JSX(javascript xml)
 }
 
 export default App;
--- React project/my_app/src/index.js
+++ React project/my_app/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+    <App />  //this app is app.js file so whatever we write in app.js file is getting rendered
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
